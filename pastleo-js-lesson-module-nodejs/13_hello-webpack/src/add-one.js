c30946477cd55436e4d604790e8a7178f64334fa
Henry <vinsen1109@gmail.com>
2020-12-17 17:50:13 +0800

    dec17

--- pastleo-js-lesson-module-nodejs/13_hello-webpack/src/add-one.js
+++ pastleo-js-lesson-module-nodejs/13_hello-webpack/src/add-one.js
@@ -1,24 +1,38 @@
+import Typed from 'typed.js';
+
 import { timeoutPromise, removeGreeting } from './utils.js';
 
 export const addOne = async ms => {
-  const timeDiv = document.createElement('div');
-  const textSpan = document.createElement('span');
+    const timeDiv = document.createElement('div');
+    const textSpan = document.createElement('span');
 
-  textSpan.textContent = 'Loading...';
+    const loadingTyped = new Typed(textSpan, {
+        strings: ['Loading...'],
+        typeSpeed: 60
+    });
 
-  timeDiv.appendChild(textSpan);
-  document.getElementById('container').appendChild(timeDiv);
+    timeDiv.appendChild(textSpan);
+    document.getElementById('container').appendChild(timeDiv);
 
-  await timeoutPromise(ms);
+    await timeoutPromise(ms);
+    loadingTyped.destroy();
+    //  顯示完刪除
 
-  textSpan.textContent = (new Date()).toString();
+    const dateTyped = new Typed(textSpan, {
+        strings: ['Completed!', (new Date()).toString()],
+        typeSpeed: 30,
+        onComplete: () => {
+            // 完成後移除游標
+            dateTyped.cursor.remove();
+        }
+    })
 }
 
 const mountAddOne = () => {
-  document.getElementById('add-one').addEventListener('click', () => {
-    removeGreeting()
-    addOne(Math.random() * 1000);
-  })
+    document.getElementById('add-one').addEventListener('click', () => {
+        removeGreeting()
+        addOne(Math.random() * 1000);
+    })
 }
 
 export default mountAddOne;
